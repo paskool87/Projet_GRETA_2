b4815ac8b593e35c297e384be10e4c753b7d1d29
pascal <pascalfebury@yahoo.fr>
2026-04-10 09:22:10 +0200

    variables css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
