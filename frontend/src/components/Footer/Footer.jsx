658e5e37ec1a1a6457eaeba362a3237500a2c9ae
pascal <pascalfebury@yahoo.fr>
2026-04-10 11:03:27 +0200

    layout header+footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <>
       <div className="footer">
-        
+        2026 GRETA Uzurat
       </div>
     </>
   );
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-
+import logos from "../../assets/images/logosGreta.png";
 import "./Header.scss";
 
 function Header() {
   return (
     <>
       <div className="header">
-        
+        <img src={logos} alt="logos" className="header__logos" />
       </div>
     </>
   );
