39dfc2cf43ff317474ca5d7bc624e8bb29a020c9
pascal <pascalfebury@yahoo.fr>
2026-05-13 15:09:01 +0200

    PastWeeks filtre par statut

--- frontend/src/pages/PastWeeks/index.jsx
+++ frontend/src/pages/PastWeeks/index.jsx
@@ -17,6 +17,7 @@ function PastWeeks() {
   const { alternantId } = useParams();
   const saveLocation = useSaveLocation("fiches");
 
+  const [statut, setStatut] = useState("");
   const [mois, setMois] = useState("");
   const [annee, setAnnee] = useState("");
 
@@ -47,17 +48,20 @@ function PastWeeks() {
     : [];
   console.log("Fiches de l'alternant :", fiches);
 
-  const fichesFiltrees = fiches.filter((fiche) => {
-    const date = new Date(fiche.date_debut);
+  const fichesFiltrees = fiches
+    .filter((fiche) => {
+      const date = new Date(fiche.date_debut);
 
-    const ficheMois = String(date.getMonth() + 1).padStart(2, "0");
-    const ficheAnnee = String(date.getFullYear());
+      const ficheMois = String(date.getMonth() + 1).padStart(2, "0");
+      const ficheAnnee = String(date.getFullYear());
 
-    const matchMois = mois ? ficheMois === mois : true;
-    const matchAnnee = annee ? ficheAnnee === annee : true;
+      const matchMois = mois ? ficheMois === mois : true;
+      const matchAnnee = annee ? ficheAnnee === annee : true;
+      const matchStatut = statut ? fiche.status_fiche === statut : true;
 
-    return matchMois && matchAnnee;
-  }).reverse();
+      return matchMois && matchAnnee && matchStatut;
+    })
+    .reverse();
 
   return (
     <>
@@ -73,6 +77,59 @@ function PastWeeks() {
             {alternantArray[0]?.alternant.utilisateur.nom}
           </h3>
         )}
+
+        <div className="filters">
+          <select
+            value={mois}
+            onChange={(e) => setMois(e.target.value)}
+            className="filters-month"
+          >
+            <option value="">Tous les mois</option>
+            <option value="01">Janvier</option>
+            <option value="02">Février</option>
+            <option value="03">Mars</option>
+            <option value="04">Avril</option>
+            <option value="05">Mai</option>
+            <option value="06">Juin</option>
+            <option value="07">Juillet</option>
+            <option value="08">Août</option>
+            <option value="09">Septembre</option>
+            <option value="10">Octobre</option>
+            <option value="11">Novembre</option>
+            <option value="12">Décembre</option>
+          </select>
+
+          <select
+            value={annee}
+            onChange={(e) => setAnnee(e.target.value)}
+            className="filters-year"
+          >
+            <option value="">années</option>
+            <option value="2026">2026</option>
+            <option value="2027">2027</option>
+            <option value="2028">2028</option>
+            <option value="2029">2029</option>
+            <option value="2030">2030</option>
+            <option value="2031">2031</option>
+            <option value="2032">2032</option>
+            <option value="2033">2033</option>
+            <option value="2034">2034</option>
+            <option value="2035">2035</option>
+            <option value="2036">2036</option>
+            <option value="2037">2037</option>
+            <option value="2038">2038</option>
+            <option value="2039">2039</option>
+            <option value="2040">2040</option>
+          </select>
+
+          <select value={statut} onChange={(e) => setStatut(e.target.value)}>
+            <option value="">Tous les statuts</option>
+            <option value="VALIDE">Validée</option>
+            <option value="SOUMISE">En attente</option>
+            <option value="BROUILLON">Brouillon</option>
+            <option value="CRITERES_NON_REMPLIS">Critères non remplis</option>
+          </select>
+        </div>
 
         <div className="past-weeks-list">
           {fiches.length === 0 ? (
@@ -99,51 +156,6 @@ function PastWeeks() {
               </Link>
             ))
           )}
-
-          <div className="filters">
-            <select
-              value={mois}
-              onChange={(e) => setMois(e.target.value)}
-              className="filters-month"
-            >
-              <option value="">Tous les mois</option>
-              <option value="01">Janvier</option>
-              <option value="02">Février</option>
-              <option value="03">Mars</option>
-              <option value="04">Avril</option>
-              <option value="05">Mai</option>
-              <option value="06">Juin</option>
-              <option value="07">Juillet</option>
-              <option value="08">Août</option>
-              <option value="09">Septembre</option>
-              <option value="10">Octobre</option>
-              <option value="11">Novembre</option>
-              <option value="12">Décembre</option>
-            </select>
-
-            <select
-              value={annee}
-              onChange={(e) => setAnnee(e.target.value)}
-              className="filters-year"
-            >
-              <option value="">années</option>
-              <option value="2026">2026</option>
-              <option value="2027">2027</option>
-              <option value="2028">2028</option>
-              <option value="2029">2029</option>
-              <option value="2030">2030</option>
-              <option value="2031">2031</option>
-              <option value="2032">2032</option>
-              <option value="2033">2033</option>
-              <option value="2034">2034</option>
-              <option value="2035">2035</option>
-              <option value="2036">2036</option>
-              <option value="2037">2037</option>
-              <option value="2038">2038</option>
-              <option value="2039">2039</option>
-              <option value="2040">2040</option>
-            </select>
-          </div>
         </div>
       </div>
     </>
